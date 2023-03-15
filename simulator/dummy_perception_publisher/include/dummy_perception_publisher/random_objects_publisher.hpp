089b4c11f62af9ad06e14e0d84afc8d6e33ff4ea
Takayuki Murooka <takayuki5168@gmail.com>
2023-03-15 19:52:55 +0900

    po

---
+++ simulator/dummy_perception_publisher/include/dummy_perception_publisher/random_objects_publisher.hpp
@@ -0,0 +1,87 @@
+// Copyright 2020 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DUMMY_PERCEPTION_PUBLISHER__RANDOM_OBJECTS_PUBLISHER_HPP_
+#define DUMMY_PERCEPTION_PUBLISHER__RANDOM_OBJECTS_PUBLISHER_HPP_
+
+#include <lanelet2_extension/utility/message_conversion.hpp>
+#include <lanelet2_extension/utility/query.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <autoware_auto_mapping_msgs/msg/had_map_bin.hpp>
+#include <autoware_auto_perception_msgs/msg/detected_objects.hpp>
+#include <autoware_auto_planning_msgs/msg/path.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <tier4_perception_msgs/msg/detected_objects_with_feature.hpp>
+
+#include <lanelet2_core/LaneletMap.h>
+#include <lanelet2_core/geometry/BoundingBox.h>
+#include <lanelet2_core/geometry/Lanelet.h>
+#include <lanelet2_core/geometry/Point.h>
+#include <lanelet2_routing/RoutingGraph.h>
+#include <lanelet2_traffic_rules/TrafficRulesFactory.h>
+
+#include <memory>
+#include <random>
+#include <vector>
+
+class VehicleObject
+{
+public:
+  VehicleObject(
+    const lanelet::ConstLanelet & start_lanelet, const geometry_msgs::msg::Pose & initial_pose,
+    const double initial_vel)
+  : nearest_lanelet(start_lanelet), pose(initial_pose), velocity(initial_vel)
+  {
+  }
+
+  lanelet::ConstLanelet nearest_lanelet;
+  geometry_msgs::msg::Pose pose;
+  double velocity;
+};
+
+class RandomObjectsPublisher : public rclcpp::Node
+{
+public:
+  RandomObjectsPublisher();
+
+private:
+  std::vector<VehicleObject> vehicle_objects_;
+  double update_hz_;
+
+  rclcpp::Subscription<autoware_auto_planning_msgs::msg::Path>::SharedPtr path_sub_;
+  rclcpp::Subscription<autoware_auto_mapping_msgs::msg::HADMapBin>::SharedPtr map_sub_;
+
+  autoware_auto_planning_msgs::msg::Path::ConstSharedPtr path_ptr_;
+
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_pub_;
+  rclcpp::Publisher<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr
+    detected_object_with_feature_pub_;
+
+  // Lanelet Map Pointers
+  std::shared_ptr<lanelet::LaneletMap> lanelet_map_ptr_;
+  std::shared_ptr<lanelet::routing::RoutingGraph> routing_graph_ptr_;
+  std::shared_ptr<lanelet::traffic_rules::TrafficRules> traffic_rules_ptr_;
+  lanelet::ConstLanelets lanelets_;
+
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  void onMap(const autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr msg);
+  void onTimer();
+  void spawnVehicleObject();
+  void step();
+  void validateObject();
+};
+
+#endif  // DUMMY_PERCEPTION_PUBLISHER__RANDOM_OBJECTS_PUBLISHER_HPP_
---
+++ simulator/dummy_perception_publisher/src/random_objects_publisher.cpp
@@ -0,0 +1,242 @@
+// Copyright 2020 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <dummy_perception_publisher/random_objects_publisher.hpp>
+#include <motion_utils/motion_utils.hpp>
+#include <tier4_autoware_utils/tier4_autoware_utils.hpp>
+
+#include <functional>
+#include <limits>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace
+{
+geometry_msgs::msg::Pose getRandomPose()
+{
+  std::mt19937 random_generator;
+  std::random_device seed_gen;
+  random_generator.seed(seed_gen());
+
+  std::normal_distribution<> pos_random(0.0, 100.0);
+  std::normal_distribution<> yaw_random(0.0, 1.0);
+
+  geometry_msgs::msg::Pose pose;
+  pose.position.x = pos_random(random_generator);
+  pose.position.y = pos_random(random_generator);
+  pose.position.z = pos_random(random_generator);
+  pose.orientation = tier4_autoware_utils::createQuaternionFromYaw(yaw_random(random_generator));
+
+  return pose;
+}
+
+geometry_msgs::msg::Pose createCenterPose(const lanelet::ConstLanelet & lanelet)
+{
+  const auto centerline = lanelet.centerline();
+  const size_t middle_point_idx = std::floor(centerline.size() / 2.0);
+
+  // get middle position of the lanelet
+  geometry_msgs::msg::Point middle_pos;
+  middle_pos.x = centerline[middle_point_idx].x();
+  middle_pos.y = centerline[middle_point_idx].y();
+
+  // get next middle position of the lanelet
+  geometry_msgs::msg::Point next_middle_pos;
+  next_middle_pos.x = centerline[middle_point_idx + 1].x();
+  next_middle_pos.y = centerline[middle_point_idx + 1].y();
+
+  // calculate middle pose
+  geometry_msgs::msg::Pose middle_pose;
+  middle_pose.position = middle_pos;
+  const double yaw = tier4_autoware_utils::calcAzimuthAngle(middle_pos, next_middle_pos);
+  middle_pose.orientation = tier4_autoware_utils::createQuaternionFromYaw(yaw);
+
+  return middle_pose;
+}
+
+lanelet::ConstLanelet extractRandomLanelet(const lanelet::ConstLanelets & lanelets)
+{
+  std::uniform_int_distribution<> index_random(0, lanelets.size());
+  std::mt19937 random_generator;
+  const size_t idx = static_cast<size_t>(index_random(random_generator));
+  return lanelets.at(idx);
+}
+
+template <typename T>
+size_t findNearestSegmentIndex(const T & points, const geometry_msgs::msg::Pose & pose)
+{
+  const auto seg_idx = motion_utils::findNearestSegmentIndex(points, pose);
+  if (seg_idx) {
+    return *seg_idx;
+  }
+  return motion_utils::findNearestSegmentIndex(points, pose.position);
+}
+
+std::vector<geometry_msgs::msg::Point> convertToPoints(
+  const lanelet::ConstLineString3d & centerline)
+{
+  std::vector<geometry_msgs::msg::Point> points;
+  for (size_t i = 0; i < centerline.size(); ++i) {
+    geometry_msgs::msg::Point point;
+    point.x = centerline[i].basicPoint().x();
+    point.y = centerline[i].basicPoint().y();
+    point.z = centerline[i].basicPoint().z();
+    points.push_back(point);
+  }
+  return points;
+}
+
+std::vector<geometry_msgs::msg::Pose> convertToPoses(const lanelet::ConstLineString3d & centerline)
+{
+  const auto points = convertToPoints(centerline);
+
+  std::vector<geometry_msgs::msg::Pose> poses;
+  for (size_t i = 0; i < points.size(); ++i) {
+    geometry_msgs::msg::Pose pose;
+    pose.position = points.at(i);
+
+    const size_t prev_idx = i == 0 ? i : i - 1;
+    const size_t next_idx = i == 0 ? i + 1 : i;
+    const double yaw =
+      tier4_autoware_utils::calcAzimuthAngle(points.at(next_idx), points.at(prev_idx));
+    pose.orientation = tier4_autoware_utils::createQuaternionFromYaw(yaw);
+    poses.push_back(pose);
+  }
+  return poses;
+}
+}  // namespace
+
+RandomObjectsPublisher::RandomObjectsPublisher() : Node("random_objects_publisher")
+{
+  path_sub_ = create_subscription<autoware_auto_planning_msgs::msg::Path>(
+    "/planning/scenario_planning/behavior_planning/path", 1,
+    [this](const autoware_auto_planning_msgs::msg::Path::ConstSharedPtr msg) { path_ptr_ = msg; });
+  map_sub_ = this->create_subscription<autoware_auto_mapping_msgs::msg::HADMapBin>(
+    "/vector_map", rclcpp::QoS{1}.transient_local(),
+    std::bind(&RandomObjectsPublisher::onMap, this, std::placeholders::_1));
+
+  detected_object_with_feature_pub_ =
+    this->create_publisher<tier4_perception_msgs::msg::DetectedObjectsWithFeature>(
+      "/perception/object_recognition/detection/objects_with_feature", 1);
+  pointcloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+    "/perception/obstacle_segmentation/pointcloud", 1);
+
+  update_hz_ = 10.0;
+  const auto period_ns = rclcpp::Rate(update_hz_).period();
+  timer_ = rclcpp::create_timer(
+    this, get_clock(), period_ns, std::bind(&RandomObjectsPublisher::onTimer, this));
+}
+
+void RandomObjectsPublisher::onMap(
+  const autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr msg)
+{
+  const auto lanelet_map_ptr = std::make_shared<lanelet::LaneletMap>();
+  lanelet::utils::conversion::fromBinMsg(
+    *msg, lanelet_map_ptr, &traffic_rules_ptr_, &routing_graph_ptr_);
+  const auto lanelets = lanelet::utils::query::laneletLayer(lanelet_map_ptr);
+  lanelets_.insert(lanelets_.end(), lanelets.begin(), lanelets.end());
+}
+
+void RandomObjectsPublisher::onTimer()
+{
+  const size_t max_object_num = 10;
+
+  if (vehicle_objects_.size() < max_object_num) {
+    spawnVehicleObject();
+  }
+
+  step();
+  validateObject();
+
+  tier4_perception_msgs::msg::DetectedObjectsWithFeature objects_msg;
+  objects_msg.header.frame_id = "base_link";
+  objects_msg.header.stamp = now();
+  for (const auto & object : vehicle_objects_) {
+    tier4_perception_msgs::msg::DetectedObjectWithFeature feature_object;
+
+    autoware_auto_perception_msgs::msg::ObjectClassification classification;
+    classification.label = autoware_auto_perception_msgs::msg::ObjectClassification::CAR;
+    feature_object.object.classification.push_back(classification);
+
+    feature_object.object.shape.type = autoware_auto_perception_msgs::msg::Shape::BOUNDING_BOX;
+    feature_object.object.kinematics.pose_with_covariance.pose = object.pose;
+    feature_object.object.kinematics.twist_with_covariance.twist.linear.x = object.velocity;
+    objects_msg.feature_objects.push_back(feature_object);
+  }
+  detected_object_with_feature_pub_->publish(objects_msg);
+  // pointcloud_pub_->publish();
+}
+
+void RandomObjectsPublisher::spawnVehicleObject()
+{
+  const auto random_pose = getRandomPose();
+  lanelet::ConstLanelet start_lanelet;
+  const bool is_found =
+    lanelet::utils::query::getClosestLanelet(lanelets_, random_pose, &start_lanelet);
+  if (!is_found) {
+    return;
+  }
+
+  const auto initial_pose = createCenterPose(start_lanelet);
+  const double initial_vel = 10.0;
+  const auto vehicle_object = VehicleObject(start_lanelet, initial_pose, initial_vel);
+
+  vehicle_objects_.push_back(vehicle_object);
+}
+
+void RandomObjectsPublisher::step()
+{
+  for (const auto & object : vehicle_objects_) {
+    const double drive_length = object.velocity / update_hz_;
+
+    double remained_length = drive_length;
+    auto nearest_lanelet = object.nearest_lanelet;
+    while (true) {
+      const auto & centerline = convertToPoses(nearest_lanelet.centerline());
+      const size_t seg_idx = findNearestSegmentIndex(centerline, object.pose);
+      const auto offset_pose =
+        motion_utils::calcLongitudinalOffsetPose(centerline, seg_idx, remained_length);
+      if (offset_pose) {
+      } else {
+        const double total_length = motion_utils::calcArcLength(centerline);
+        const double offset =
+          motion_utils::calcSignedArcLength(centerline, 0, object.pose.position, seg_idx);
+        remained_length = remained_length - (total_length - offset);
+
+        const auto next_lanelets = routing_graph_ptr_->following(nearest_lanelet);
+        if (next_lanelets.empty()) {
+          std::cerr << "Lane ends" << std::endl;
+          break;
+        }
+        nearest_lanelet = extractRandomLanelet(next_lanelets);
+      }
+    }
+  }
+}
+
+void RandomObjectsPublisher::validateObject()
+{
+  for (size_t i = 0; i < vehicle_objects_.size(); ++i) {
+    const auto & object = vehicle_objects_.at(i);
+    const size_t seg_idx = findNearestSegmentIndex(path_ptr_->points, object.pose);
+    const double lat_offset_to_path =
+      motion_utils::calcLateralOffset(path_ptr_->points, object.pose.position, seg_idx);
+    if (100.0 < std::abs(lat_offset_to_path)) {
+      vehicle_objects_.erase(vehicle_objects_.begin() + i);
+      break;  // TODO(murooka): remove this break
+    }
+  }
+}
