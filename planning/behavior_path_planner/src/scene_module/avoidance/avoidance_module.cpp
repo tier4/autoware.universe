fa10714c82a5b1b3d1b8cd8981cd62a9d5cbc797
satoshi-ota <satoshi.ota928@gmail.com>
2023-05-26 00:11:44 +0900

    fix(avoidance): update lock offset
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
+++ planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
@@ -2841,7 +2841,7 @@ BehaviorModuleOutput AvoidanceModule::plan()
 
   // TODO(Satoshi OTA): remove this workaround
   {
-    constexpr double THRESHOLD = 0.5;
+    constexpr double THRESHOLD = 1.0;
     const size_t ego_seg_idx = planner_data_->findEgoSegmentIndex(output.path->points);
     const auto offset =
       motion_utils::calcLateralOffset(output.path->points, getEgoPosition(), ego_seg_idx);
