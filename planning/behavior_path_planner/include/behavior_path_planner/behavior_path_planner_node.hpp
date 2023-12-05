3f3d49579c89e7c43aa367cedc98037e93c08cf1
Kosuke Takeuchi <kosuke.tnp@gmail.com>
2023-12-06 02:25:54 +0900

    feat(behavior_path_planner): allow reroute when always executable module running or candidate modules is running (#5786)
    feat(behavior_path_planner): allow reroute when always executable module running or candidate modules
    
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- planning/behavior_path_planner/include/behavior_path_planner/behavior_path_planner_node.hpp
+++ planning/behavior_path_planner/include/behavior_path_planner/behavior_path_planner_node.hpp
@@ -178,7 +178,7 @@ private:
   /**
    * @brief publish reroute availability
    */
-  void publish_reroute_availability();
+  void publish_reroute_availability() const;
 
   /**
    * @brief publish steering factor from intersection
