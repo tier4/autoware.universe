d10d3e8ff77550b9eb44ea63f210b2bf34f4fc05
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-01-19 03:40:39 +0900

    feat(simple_planning_simulator): update plot_timing_diffs.py
    Signed-off-by: Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>

--- jari_rosbag_replayer/plot_timing_diffs.py
+++ jari_rosbag_replayer/plot_timing_diffs.py
@@ -12,7 +12,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     dir = args.dir
 
-    filelist = [dir + file for file in os.listdir(dir) if os.path.isfile(dir + file)]
+    # filelist = [dir + file for file in os.listdir(dir) if os.path.isfile(dir + file)]
+    filelist = [dir + file for file in os.listdir(dir) if os.path.isfile(dir + file) and file.endswith('.csv')]
+
+    averages = []
+    stds = []
 
     # read csv data from files and plot the data in the same figure
     for file in filelist:
@@ -26,22 +30,40 @@ if __name__ == '__main__':
             for line in lines:
                 # 空白（スペース、タブ）を削除
                 cleaned_line = line.replace(" ", "").replace(",", "")
-                f.write(cleaned_line + '\n')
+                if cleaned_line != "\n":
+                    f.write(cleaned_line)
 
         data = np.loadtxt(file, delimiter=',')
         data /= 1000000 # convert ns to ms
+        data = data[data < 50]
+        averages.append(np.mean(data))
+        stds.append(np.std(data))
         plt.plot(data, label=file)
     # plt.legend()
     plt.title('frame offset between control command and simulation')
     plt.ylabel('stamp difference (ms)')
     plt.xlabel('frame number')
-    plt.xlim(0, 800)
-    plt.ylim(0, 30.0)
+    plt.xlim(0, 1000)
+    # plt.ylim(0, 30.0)
     # save the figure
     plt.savefig(dir + 'stamp_diff.svg')
     # png
     plt.savefig(dir + 'stamp_diff.png')
     plt.show()
+    plt.close()
+    
+    # plot the averages as x and stds as y
+    plt.plot(averages, stds, 'o')
+    plt.title('averages and stds of stamp difference in each execution')
+    plt.ylabel('std [ms]')
+    plt.xlabel('average [ms]')
+    plt.xlim(0, 30.0)
+    plt.ylim(0, 15.0)
+    plt.savefig(dir +'stamp_diff_std.svg')
+    plt.savefig(dir +'stamp_diff_std.png')
+    plt.show()
+    plt.close()
+    
 
 
 
