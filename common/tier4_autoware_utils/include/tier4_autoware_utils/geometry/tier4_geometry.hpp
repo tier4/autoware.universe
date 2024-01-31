93c4d90c82cd9bfbda701b4237cbddcde7fede76
Daniel Sanchez <danielsanchezaran@gmail.com>
2024-01-31 17:12:37 +0900

    cop
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- common/tier4_autoware_utils/include/tier4_autoware_utils/geometry/tier4_geometry.hpp
+++ common/tier4_autoware_utils/include/tier4_autoware_utils/geometry/tier4_geometry.hpp
@@ -1,3 +1,16 @@
+// Copyright 2024 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 // Original implementation: https://github.com/danielsanchezaran/CPP_Convex_Hulls
 
 #ifndef TIER4_AUTOWARE_UTILS__GEOMETRY__TIER4_GEOMETRY_HPP_
