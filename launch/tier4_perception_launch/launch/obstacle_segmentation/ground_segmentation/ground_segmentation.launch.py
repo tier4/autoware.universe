8b72dbcf24b0d923bf907865f2860349e448c458
badai-nguyen <dai.nguyen@tier4.jp>
2024-04-18 10:25:16 +0900

    Revert "fix(ground_segmentation): add field select option"
    This reverts commit 36fc5541ab80270b9e6d5575a707801ee38e32e1.

--- launch/tier4_perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
+++ launch/tier4_perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
@@ -118,10 +118,7 @@ class GroundSegmentationPipeline:
                     ("output", f"{lidar_name}/pointcloud"),
                 ],
                 parameters=[
-                    self.ground_segmentation_param[f"{lidar_name}_ground_filter"]["parameters"],
-                    {"input_frame": "base_link"},
-                    {"output_frame": "base_link"},
-                    {"output_intensity_field": LaunchConfiguration("output_intensity_field")},
+                    self.ground_segmentation_param[f"{lidar_name}_ground_filter"]["parameters"]
                 ],
                 extra_arguments=[
                     {"use_intra_process_comms": LaunchConfiguration("use_intra_process")}
@@ -271,7 +268,6 @@ class GroundSegmentationPipeline:
                     self.vehicle_info,
                     {"input_frame": "base_link"},
                     {"output_frame": "base_link"},
-                    {"output_intensity_field": LaunchConfiguration("output_intensity_field")},
                 ],
                 extra_arguments=[
                     {"use_intra_process_comms": LaunchConfiguration("use_intra_process")}
@@ -567,7 +563,6 @@ def generate_launch_description():
     add_launch_arg("use_pointcloud_container", "False")
     add_launch_arg("container_name", "perception_pipeline_container")
     add_launch_arg("input/pointcloud", "/sensing/lidar/concatenated/pointcloud")
-    add_launch_arg("output_intensity_field", "False")
 
     set_container_executable = SetLaunchConfiguration(
         "container_executable",
