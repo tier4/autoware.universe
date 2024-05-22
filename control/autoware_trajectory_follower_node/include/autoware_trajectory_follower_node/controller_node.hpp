75ea9470eb25d5c0128ddaf7a489539cf2c193af
Zhe Shen <lucidshenzhe@gmail.com>
2024-05-22 13:40:10 +0900

    dependency added.
    Signed-off-by: Zhe Shen <lucidshenzhe@gmail.com>

--- control/autoware_trajectory_follower_node/include/autoware_trajectory_follower_node/controller_node.hpp
+++ control/autoware_trajectory_follower_node/include/autoware_trajectory_follower_node/controller_node.hpp
@@ -30,6 +30,7 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <tier4_autoware_utils/ros/published_time_publisher.hpp>
+#include <diagnostic_updater/diagnostic_updater.hpp>
 
 #include "autoware_control_msgs/msg/control.hpp"
 #include "autoware_control_msgs/msg/longitudinal.hpp"
@@ -73,7 +74,7 @@ private:
   double timeout_thr_sec_;
   boost::optional<LongitudinalOutput> longitudinal_output_{boost::none};
 
-  std::shared_ptr<diagnostic_updater::Updater> diag_updater_{this}; // Diagnostic updater for publishing diagnostic data.
+  std::shared_ptr<diagnostic_updater::Updater> diag_updater_ = std::make_shared<diagnostic_updater::Updater>(this); // Diagnostic updater for publishing diagnostic data.
 
   std::shared_ptr<trajectory_follower::LongitudinalControllerBase> longitudinal_controller_;
   std::shared_ptr<trajectory_follower::LateralControllerBase> lateral_controller_;
