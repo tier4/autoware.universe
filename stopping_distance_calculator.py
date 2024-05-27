985e26b306626be20fbb2daf88de1c33d647b416
Daniel Sanchez <danielsanchezaran@gmail.com>
2024-05-27 18:10:16 +0900

    update calculation
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- stopping_distance_calculator.py
+++ stopping_distance_calculator.py
@@ -1,61 +1,52 @@
 import math
+import pandas as pd
+import numpy as np
 
 
-def calculate_kinematics(start_pos, start_speed, reaction_time, jerk, target_accel):
-    # After reaction time
-    pos_after_reaction = start_pos + start_speed * reaction_time
-    speed_after_reaction = start_speed
-    time_after_reaction = reaction_time
-
-    # Time to reach target acceleration
-    time_to_reach_target_accel = target_accel / jerk
-    pos_after_target_accel = pos_after_reaction + speed_after_reaction * \
-        time_to_reach_target_accel + (1.0/6.0) * jerk * time_to_reach_target_accel**3
-    speed_after_target_accel = speed_after_reaction + 0.5 * jerk * time_to_reach_target_accel ** 2
-
-    # vehicle does not go in reverse and stops before target acc is reached
-    if speed_after_target_accel < 0.0:
-        time_to_zero_speed = math.sqrt(- 2 * speed_after_reaction / jerk)
-        pos_when_zero_speed = pos_after_reaction + speed_after_reaction * \
-            time_to_zero_speed + (1.0/6.0) * jerk * time_to_zero_speed**3
-
-        time_when_zero_speed = reaction_time + time_to_zero_speed
-        results = {
-            "position_after_reaction_time": pos_after_reaction,
-            "time_after_reaction_time": time_after_reaction,
-            "position_when_target_acceleration_reached": None,
-            "time_when_target_acceleration_reached": None,
-            "position_when_zero_speed_reached": pos_when_zero_speed,
-            "time_when_zero_speed_reached": time_when_zero_speed
-        }
-        return results
-
-    time_after_target_accel = reaction_time + time_to_reach_target_accel
-    # Time to reach 0 speed after target acceleration is reached
-    time_to_zero_speed = speed_after_target_accel / -target_accel
-    pos_when_zero_speed = pos_after_target_accel + speed_after_target_accel * \
-        time_to_zero_speed + 0.5 * target_accel * time_to_zero_speed**2
-    time_when_zero_speed = time_after_target_accel + time_to_zero_speed
-
-    results = {
-        "position_after_reaction_time": pos_after_reaction,
-        "time_after_reaction_time": time_after_reaction,
-        "position_when_target_acceleration_reached": pos_after_target_accel,
-        "time_when_target_acceleration_reached": time_after_target_accel,
-        "position_when_zero_speed_reached": pos_when_zero_speed,
-        "time_when_zero_speed_reached": time_when_zero_speed
-    }
-
-    return results
+class Params:
+    def __init__(self, pos=0, speed=0, reaction_time=0, accel=0):
+        self.pos = pos
+        self.speed = speed
+        self.reaction_time = reaction_time
+        self.accel = accel
 
 
-# Example usage
-start_pos = 0.0        # Starting position in meters
-start_speed = 20.0     # Starting speed in m/s
-reaction_time = 1.0    # Reaction time in seconds
-jerk = -2.0             # Jerk in m/s^3
-target_accel = -4.0    # Target acceleration in m/s^2
+def calculate_collision_with_rss(ego_params: Params, front_vehicle_params: Params, rear_vehicle_params: Params):
+    ego_pos, ego_speed, rt, ego_acc = ego_params.pos, ego_params.speed, ego_params.reaction_time, ego_params.accel
 
-results = calculate_kinematics(start_pos, start_speed, reaction_time, jerk, target_accel)
-for key, value in results.items():
-    print(f"{key}: {value:.2f}")
+    front_pos, front_speed, _, front_acc = front_vehicle_params.pos, front_vehicle_params.speed, front_vehicle_params.reaction_time, front_vehicle_params.accel
+    is_front_collision = front_pos - ego_pos < ego_speed * rt + \
+        (ego_speed ** 2 / (-2.0 * ego_acc)) - (front_speed ** 2) / (-2*front_acc)
+
+    rear_pos, rear_speed, rt, rear_acc = rear_vehicle_params.pos, rear_vehicle_params.speed, rear_vehicle_params.reaction_time, rear_vehicle_params.accel
+    is_rear_collision = ego_pos - rear_pos < rear_speed * rt + \
+        (rear_speed ** 2 / (-2.0 * rear_acc)) - (ego_speed ** 2) / (-2*ego_acc)
+
+    return is_front_collision, is_rear_collision
+
+
+# Set the parameters for front and rear vehicles
+front_vehicle_params = Params(pos=30, speed=10, reaction_time=1.5, accel=-2.0)
+rear_vehicle_params = Params(pos=-30, speed=10, reaction_time=1.5, accel=-2.0)
+
+# Generate the data for the table
+speeds = np.arange(1, 31)  # 1 m/s to 30 m/s
+accelerations = np.linspace(-0.5, -10, 20)  # -0.5 m/s² to -10 m/s²
+
+collision_table = pd.DataFrame(index=accelerations, columns=speeds)
+
+for speed in speeds:
+    for accel in accelerations:
+        ego_params = Params(pos=0, speed=speed, reaction_time=1.5, accel=accel)
+        is_front_collision, is_rear_collision = calculate_collision_with_rss(
+            ego_params, front_vehicle_params, rear_vehicle_params)
+        if is_front_collision and is_rear_collision:
+            collision_table.at[accel, speed] = "Both"
+        elif is_front_collision:
+            collision_table.at[accel, speed] = "Front"
+        elif is_rear_collision:
+            collision_table.at[accel, speed] = "Rear"
+        else:
+            collision_table.at[accel, speed] = "None"
+
+print(collision_table)
