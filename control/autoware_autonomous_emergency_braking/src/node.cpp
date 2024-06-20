98cc3b81adefa7a2f49ca63b7605ccc9c01fbc49
Daniel Sanchez <danielsanchezaran@gmail.com>
2024-06-20 12:08:14 +0900

    remove state dependency
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- control/autoware_autonomous_emergency_braking/src/node.cpp
+++ control/autoware_autonomous_emergency_braking/src/node.cpp
@@ -357,10 +357,10 @@ bool AEB::fetchLatestData()
     return missing("control predicted trajectory");
   }
 
-  autoware_state_ = sub_autoware_state_.takeData();
-  if (!autoware_state_) {
-    return missing("autoware_state");
-  }
+  // autoware_state_ = sub_autoware_state_.takeData();
+  // if (!autoware_state_) {
+  //   return missing("autoware_state");
+  // }
 
   return true;
 }
@@ -399,9 +399,9 @@ bool AEB::checkCollision(MarkerArray & debug_markers)
   }
 
   // if not driving, disable aeb
-  if (autoware_state_->state != AutowareState::DRIVING) {
-    return false;
-  }
+  // if (autoware_state_->state != AutowareState::DRIVING) {
+  //   return false;
+  // }
 
   // step2. create velocity data check if the vehicle stops or not
   constexpr double min_moving_velocity_th{0.1};
