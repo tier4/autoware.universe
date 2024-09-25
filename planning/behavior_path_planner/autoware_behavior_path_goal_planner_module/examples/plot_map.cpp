247f389da57cea96085d051869dec120785763f4
Mamoru Sobue <mamoru.sobue@tier4.jp>
2024-09-25 13:22:11 +0900

    demo
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_goal_planner_module/examples/plot_map.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_goal_planner_module/examples/plot_map.cpp
@@ -34,11 +34,87 @@
 
 #include <lanelet2_io/Io.h>
 #include <matplotlibcpp17/pyplot.h>
+#include <yaml-cpp/yaml.h>
 
 #include <chrono>
 #include <iostream>
 
 using namespace std::chrono_literals;  // NOLINT
+
+namespace
+{
+geometry_msgs::msg::Pose parse_pose(const YAML::Node & node)
+{
+  geometry_msgs::msg::Pose pose;
+  pose.position.x = node["position"]["x"].as<double>();
+  pose.position.y = node["position"]["y"].as<double>();
+  pose.position.z = node["position"]["z"].as<double>();
+  pose.orientation.x = node["orientation"]["x"].as<double>();
+  pose.orientation.y = node["orientation"]["y"].as<double>();
+  pose.orientation.z = node["orientation"]["z"].as<double>();
+  pose.orientation.w = node["orientation"]["w"].as<double>();
+  return pose;
+}
+
+autoware_planning_msgs::msg::LaneletPrimitive parse_lanelet_primitive(const YAML::Node & node)
+{
+  autoware_planning_msgs::msg::LaneletPrimitive primitive;
+  primitive.id = node["id"].as<int64_t>();
+  primitive.primitive_type = node["primitive_type"].as<std::string>();
+
+  return primitive;
+}
+
+std::vector<autoware_planning_msgs::msg::LaneletPrimitive> parse_lanelet_primitives(
+  const YAML::Node & node)
+{
+  std::vector<autoware_planning_msgs::msg::LaneletPrimitive> primitives;
+  primitives.reserve(node.size());
+  std::transform(node.begin(), node.end(), std::back_inserter(primitives), [&](const auto & p) {
+    return parse_lanelet_primitive(p);
+  });
+
+  return primitives;
+}
+
+std::vector<autoware_planning_msgs::msg::LaneletSegment> parse_segments(const YAML::Node & node)
+{
+  std::vector<autoware_planning_msgs::msg::LaneletSegment> segments;
+  std::transform(node.begin(), node.end(), std::back_inserter(segments), [&](const auto & input) {
+    autoware_planning_msgs::msg::LaneletSegment segment;
+    segment.preferred_primitive = parse_lanelet_primitive(input["preferred_primitive"]);
+    segment.primitives = parse_lanelet_primitives(input["primitives"]);
+    return segment;
+  });
+
+  return segments;
+}
+}  // namespace
+
+void loadFromYAML(
+  std::shared_ptr<autoware::behavior_path_planner::PlannerData> planner_data,
+  const std::string & planner_data_yaml_path)
+{
+  const auto planner_data_yaml = YAML::LoadFile(planner_data_yaml_path);
+
+  const auto route_node = planner_data_yaml["route"];
+  autoware_planning_msgs::msg::LaneletRoute route_msg;
+  {
+    route_msg.start_pose = ::parse_pose(route_node["start_pose"]);
+    route_msg.goal_pose = ::parse_pose(route_node["goal_pose"]);
+    route_msg.segments = ::parse_segments(route_node["segments"]);
+    route_msg.allow_modification = route_node["allow_modification"].as<bool>();
+  }
+  planner_data->route_handler->setRoute(route_msg);
+
+  const auto self_odometry_node = planner_data_yaml["self_odometry"];
+  auto odom_msg = std::make_shared<nav_msgs::msg::Odometry>();
+  {
+    odom_msg->pose.pose = ::parse_pose(self_odometry_node["pose"]["pose"]);
+    // TODO(soblin): covariance, twist, accel, etc.
+  }
+  planner_data->self_odometry = odom_msg;
+}
 
 void plot_path_with_lane_id(
   matplotlibcpp17::axes::Axes & axes, const tier4_planning_msgs::msg::PathWithLaneId path)
@@ -101,116 +177,10 @@ int main(int argc, char ** argv)
     return 1;
   }
 
-  autoware_planning_msgs::msg::LaneletRoute route_msg;
-  route_msg.start_pose = geometry_msgs::build<geometry_msgs::msg::Pose>()
-                           .position(geometry_msgs::build<geometry_msgs::msg::Point>()
-                                       .x(530.707103865218)
-                                       .y(436.8758309382301)
-                                       .z(100.0))
-                           .orientation(geometry_msgs::build<geometry_msgs::msg::Quaternion>()
-                                          .x(0.0)
-                                          .y(0.0)
-                                          .z(0.2187392230193602)
-                                          .w(0.9757833531644647));
-  route_msg.goal_pose = geometry_msgs::build<geometry_msgs::msg::Pose>()
-                          .position(geometry_msgs::build<geometry_msgs::msg::Point>()
-                                      .x(571.8018955394537)
-                                      .y(435.6819504507543)
-                                      .z(100.0))
-                          .orientation(geometry_msgs::build<geometry_msgs::msg::Quaternion>()
-                                         .x(0.0)
-                                         .y(0.0)
-                                         .z(-0.3361155457734493)
-                                         .w(0.9418207578352774));
-  /*
-  route_msg.goal_pose =
-    geometry_msgs::build<geometry_msgs::msg::Pose>()
-      .position(
-        geometry_msgs::build<geometry_msgs::msg::Point>().x(568.836731).y(437.871904).z(100.0))
-      .orientation(
-        geometry_msgs::build<geometry_msgs::msg::Quaternion>().x(0.0).y(0.0).z(-0.292125).w(
-          0.956380));
-  */
-
-  route_msg.segments = std::vector<autoware_planning_msgs::msg::LaneletSegment>{
-    autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletSegment>()
-      .preferred_primitive(
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(17757)
-          .primitive_type(""))
-      .primitives(std::vector<autoware_planning_msgs::msg::LaneletPrimitive>{
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(17756)
-          .primitive_type("lane"),
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(17757)
-          .primitive_type("lane"),
-      }),
-    autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletSegment>()
-      .preferred_primitive(
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(18494)
-          .primitive_type(""))
-      .primitives(std::vector<autoware_planning_msgs::msg::LaneletPrimitive>{
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(18494)
-          .primitive_type("lane"),
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(18493)
-          .primitive_type("lane"),
-      }),
-    autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletSegment>()
-      .preferred_primitive(
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(18496)
-          .primitive_type(""))
-      .primitives(std::vector<autoware_planning_msgs::msg::LaneletPrimitive>{
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(18496)
-          .primitive_type("lane"),
-        autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletPrimitive>()
-          .id(18497)
-          .primitive_type("lane"),
-      }),
-  };
-  route_msg.allow_modification = false;
   autoware_map_msgs::msg::LaneletMapBin map_bin;
   lanelet::utils::conversion::toBinMsg(
     lanelet_map_ptr, &map_bin);  // TODO(soblin): pass lanelet_map_ptr to RouteHandler
 
-  /*
-    reference:
-    https://github.com/ros2/rclcpp/blob/ee94bc63e4ce47a502891480a2796b53d54fcdfc/rclcpp/test/rclcpp/test_parameter_client.cpp#L927
-   */
-  /*
-  auto node = rclcpp::Node::make_shared(
-    "node", "namespace", rclcpp::NodeOptions().allow_undeclared_parameters(true));
-  auto param_node = std::make_shared<rclcpp::AsyncParametersClient>(node, "");
-  {
-    auto future = param_node->load_parameters(
-      ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner") +
-      "/config/behavior_path_planner.param.yaml");
-    if (rclcpp::spin_until_future_complete(node, future) != rclcpp::FutureReturnCode::SUCCESS) {
-      RCLCPP_INFO(node->get_logger(), "failed to load behavior_path_planner.param.yaml");
-    }
-  }
-  {
-    auto future = param_node->load_parameters(
-      ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner") +
-      "/config/drivable_area_expansion.param.yaml");
-    if (rclcpp::spin_until_future_complete(node, future) != rclcpp::FutureReturnCode::SUCCESS) {
-      RCLCPP_INFO(node->get_logger(), "failed to load drivable_area_expansion.param.yaml");
-    }
-  }
-  {
-    auto future = param_node->load_parameters(
-      ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner") +
-      "/config/scene_module_manager.param.yaml");
-    if (rclcpp::spin_until_future_complete(node, future) != rclcpp::FutureReturnCode::SUCCESS) {
-      RCLCPP_INFO(node->get_logger(), "failed to load scene_module_manager.param.yaml");
-    }
-  }
-  */
   auto node_options = rclcpp::NodeOptions{};
   node_options.parameter_overrides(
     std::vector<rclcpp::Parameter>{{"launch_modules", std::vector<std::string>{}}});
@@ -238,34 +208,42 @@ int main(int argc, char ** argv)
       "/config/goal_planner.param.yaml"});
   auto node = rclcpp::Node::make_shared("plot_map", node_options);
 
+  // init PlannerData from saved file
   auto planner_data = std::make_shared<autoware::behavior_path_planner::PlannerData>();
   planner_data->init_parameters(*node);
   planner_data->route_handler->setMap(map_bin);
-  planner_data->route_handler->setRoute(route_msg);
-  nav_msgs::msg::Odometry odom;
-  odom.pose.pose = route_msg.start_pose;
-  auto odometry = std::make_shared<const nav_msgs::msg::Odometry>(odom);
-  planner_data->self_odometry = odometry;
+  loadFromYAML(
+    planner_data, ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner") +
+                    "/config/planner_data.yaml");
+
+  // get current_route lanelet
   lanelet::ConstLanelet current_route_lanelet;
   planner_data->route_handler->getClosestLaneletWithinRoute(
-    route_msg.start_pose, &current_route_lanelet);
+    planner_data->self_odometry->pose.pose, &current_route_lanelet);
   std::cout << "current_route_lanelet is " << current_route_lanelet.id() << std::endl;
+
+  // generate reference path
   const auto reference_path =
     autoware::behavior_path_planner::utils::getReferencePath(current_route_lanelet, planner_data);
-  auto goal_planner_parameter =
-    autoware::behavior_path_planner::GoalPlannerModuleManager::initGoalPlannerParameters(
-      node.get(), "goal_planner.");
+
+  // generate vehicle_info
   const auto vehicle_info = autoware::vehicle_info_utils::VehicleInfoUtils(*node).getVehicleInfo();
+
   autoware::lane_departure_checker::LaneDepartureChecker lane_departure_checker{};
   lane_departure_checker.setVehicleInfo(vehicle_info);
   autoware::lane_departure_checker::Param lane_departure_checker_params;
+  auto goal_planner_parameter =
+    autoware::behavior_path_planner::GoalPlannerModuleManager::initGoalPlannerParameters(
+      node.get(), "goal_planner.");
   lane_departure_checker_params.footprint_extra_margin =
     goal_planner_parameter.lane_departure_check_expansion_margin;
   lane_departure_checker.setParam(lane_departure_checker_params);
+
+  // geneate ShiftPullOverPlanner and pull_over_path
   auto shift_pull_over_planner = autoware::behavior_path_planner::ShiftPullOver(
     *node, goal_planner_parameter, lane_departure_checker);
-  const auto pull_over_path_opt =
-    shift_pull_over_planner.plan(planner_data, reference_path, route_msg.goal_pose);
+  const auto pull_over_path_opt = shift_pull_over_planner.plan(
+    planner_data, reference_path, planner_data->route_handler->getGoalPose());
 
   pybind11::scoped_interpreter guard{};
   auto plt = matplotlibcpp17::pyplot::import();
